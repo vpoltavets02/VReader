700ebce92d8da323e62bc5a37ffdb1362c103d39
Vladyslav Poltavets <vpoltavets02@gmail.com>
2024-03-27 11:55:00 +0300

    Initial commit

---
+++ src/main/java/v/reader/VReaderApplication.java
@@ -0,0 +1,13 @@
+package v.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/v/reader/VReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package v.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
